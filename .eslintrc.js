ee3960ea0e8f5f02d2612abdb7d7cf170a17c9fb
Denes Dolhay <denke@denkesys.com>
2022-09-18 13:24:56 -0400

    Update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,8 @@ module.exports = {
     ecmaVersion: 2021,
   },
   rules: {
-    "import/prefer-default-export": ["off"],
-    "prefer-object-spread": ["off"],
     "no-plusplus": ["off"],
     "arrow-body-style": ["off"],
-    "new-cap": ["off"],
     "no-console": ["off"],
   },
   env: {
